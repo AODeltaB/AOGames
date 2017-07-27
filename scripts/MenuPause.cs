e4b37af7a1c036f2e2a7510f764ffa1b4567be3b
Pierre <aodeltab@gmail.com>
2017-07-27 23:13:43 +0200

    Add files via upload

---
+++ scripts/MenuPause.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MenuPause : MonoBehaviour {
+
+
+public static bool isActive = false;
+public GameObject menu_Button;
+public AudioSource audioSource;
+public Transform Player;
+
+
+void Update () {
+
+    if(Input.GetKeyDown(KeyCode.Escape))
+    {
+        RESUME_BUTTON();
+    }
+    
+    if(isActive == true) //  && lifecounter_pb.PlayerIsDead == false
+    {
+        Player.GetComponent<FirstPersonController>().enabled = false;   // Désactive le FPS Controller
+        audioSource.mute = true; // Mute la musique
+        menu_Button.SetActive(true); // Affiche le Menu Pause
+        Cursor.visible = true; // Affiche le curseur
+        Cursor.lockState = CursorLockMode.Confined; // Curseur bougeable
+        Time.timeScale = 0; // Arrête les animations
+    } else {
+        Player.GetComponent<FirstPersonController>().enabled = true;   // Réactive le FPS Controller
+       	audioSource.mute = false; // Unmute la musique
+        menu_Button.SetActive(false); // Désactive le menu Pause
+        Cursor.visible = false; // Désactive le curseur
+        Cursor.lockState = CursorLockMode.Locked; // Bloque le curseur au milieu de l'écran
+        Time.timeScale = 1; // Joue les animations
+    }
+   
+}
+    
+    public void RESUME_BUTTON() {
+    isActive = !isActive;
+}
+
+}
+
--- scripts/lifecounter_pb.cs
+++ scripts/lifecounter_pb.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class lifecounter_pb : MonoBehaviour
 {
@@ -7,15 +8,16 @@ public class lifecounter_pb : MonoBehaviour
     public Slider LifeBar; // Notre Slider LifeBar
     public Text life_progressBarText; // Le texte value de la vie
     public GameObject DeathScreen; // Pannel
-
+    public Transform Player;
     // Variables d'initialisation
-    public float startingHealth = 100; // Comme dans tous jeux, la vie est set à 100
-    public float currentHealth; // oyeah
-    public bool PlayerIsDead = false; // Variable booléenne pour dire si le joueur est mort ou non
+    private float startingHealth = 50; // Comme dans tous jeux, la vie est set à 100
+    private float currentHealth; // oyeah
+    public static bool PlayerIsDead = false; // Variable booléenne pour dire si le joueur est mort ou non
 
 
     void Start()
     {
+        Player.GetComponent<FirstPersonController>().enabled = true;
         float calcul_pb = Mathf.Clamp01(startingHealth / .9f); // Calcul pour mettre 100 HP a 1 value Slider
         currentHealth = calcul_pb; // On initalise currentHealth a 1
     }
@@ -35,19 +37,28 @@ public class lifecounter_pb : MonoBehaviour
             }
         }
 
-        if (currentHealth <= .0f) // test si t'es mort ou non
+        if (currentHealth <= .0f)// test si t'es mort ou non
         {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.Confined;
             PlayerIsDead = true; // Mets le joueur "mort"
+        }
+
+        if (PlayerIsDead)
+        {
+           // MenuPause.isActive = false;
+            Player.GetComponent<FirstPersonController>().enabled = false;
+            Cursor.visible = true; // Affiche le curseur
+            Cursor.lockState = CursorLockMode.Confined; // Active la possibilité de bouger le curseur          
             DeathScreen.SetActive(true); // Active l'écran noir "You died"
             Time.timeScale = 0; // Arrête les animations
         }
-        else
+        /*if (PlayerIsDead == false)
         {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Player.GetComponent<FirstPersonController>().enabled = true;
             DeathScreen.SetActive(false); // Si le joueur n'est pas mort alors on désactive l'écran
             Time.timeScale = 1; // et on joue les animations
-        }
+        }*/
         life_progressBarText.text = currentHealth * 100f + " HP"; // Récupère en string currentHealth * 100 et rajoute HP
     }
 }
